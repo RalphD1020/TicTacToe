e25bb650de149cabbfe7b5fe6ad8404ca6f2b8ca
RalphD1020 <RalphDimayugasEmail@gmail.com>
2023-09-26 09:58:16 +0800

    empty button created

---
+++ Assets/Script/TileManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileManager : MonoBehaviour
+{
+    public Button button;
+    public GameObject emptyPrefab;
+    public GameObject xPrefab;
+    public GameObject oPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Hi");
+    }
+}
