6a6c455bcfbd8112b0d3e1f5fe403041de3ed35a
RalphD1020 <RalphDimayugasEmail@gmail.com>
2023-09-26 10:05:41 +0800

    Button now can switch effectively

--- Assets/Script/TileManager.cs
+++ Assets/Script/TileManager.cs
@@ -1,28 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TileManager : MonoBehaviour
 {
+    // Reference to the button
     public Button button;
     public GameObject emptyPrefab;
     public GameObject xPrefab;
     public GameObject oPrefab;
-    // Start is called before the first frame update
-    void Start()
+    private char currentPlayer = 'X';
+    
+    public void Start()
     {
-        
+        ActivatePrefab(emptyPrefab);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Function that gets called when the button is clicked
     public void OnButtonClick()
     {
-        Debug.Log("Hi");
+        Debug.Log("Current player is: " + currentPlayer);
+        updateButtonDisplay();
+        // button.interactable = false;
+    }
+    
+    private void ActivatePrefab(GameObject prefabToActivate)
+    {
+        // Deactivate all prefabs
+        emptyPrefab.SetActive(false);
+        xPrefab.SetActive(false);
+        oPrefab.SetActive(false);
+
+        // Activate the chosen prefab
+        prefabToActivate.SetActive(true);
+    }
+
+    // todo: Define Button Display Behavior
+    public void updateButtonDisplay()
+    {
+        if (currentPlayer == 'X')
+        {
+            ActivatePrefab(xPrefab);
+            currentPlayer = 'O';
+        }
+        else
+        {
+            ActivatePrefab(oPrefab);
+            currentPlayer = 'X';
+        }
     }
 }
